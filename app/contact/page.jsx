62fe630c8eb28c5c778829d47fbde802a8fc294e
Henry James Ribano <henryjamesribano27@gmail.com>
2024-04-07 13:23:59 +0800

    finished

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,11 +1,13 @@
 import { MailIcon, HomeIcon } from "lucide-react";
+//Components
+import Form from "@/components/Form";
 
 const Contact = () => {
     return (
         <section>
             <div className="container mx-auto">
                 {/* text and illustration */}
-                <div>
+                <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-6 xl:mb-24">
                     {/* text */}
                     <div className="flex flex-col justify-center">
                         <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
@@ -23,9 +25,26 @@ const Contact = () => {
                             fringilla nec.
                         </p>
                     </div>
+                    {/* illustration */}
+                    <div className="hidden w-full xl:flex bg-contact_illustration_bg bg-contain bg-no-repeat bg-top"></div>
                 </div>
                 {/* info text and form */}
-                <div>info text and form</div>
+                <div className="grid xl:grid-cols-2 mb-24 xl:mb-32">
+                    {/* info text */}
+                    <div className="flex flex-col gap-y-4 xl:gap-y-12 mb-12 xl:mb-24 text-base xl:text-lg">
+                        {/* mail */}
+                        <div className="flex items-center gap-x-8">
+                            <MailIcon size={18} className="text-primary" />
+                            <div>henryribano27@gmail.com</div>
+                        </div>
+                        {/* home */}
+                        <div className="flex items-center gap-x-8">
+                            <HomeIcon size={18} className="text-primary" />
+                            <div>Laguile, Taal, Batangas, Philippines.</div>
+                        </div>
+                    </div>
+                    <Form />
+                </div>
             </div>
         </section>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -73,6 +73,9 @@ module.exports = {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
             },
+            backgroundImage: {
+                contact_illustration_bg: "url(/contact/contactme.png)",
+            },
         },
     },
     plugins: [require("tailwindcss-animate")],
